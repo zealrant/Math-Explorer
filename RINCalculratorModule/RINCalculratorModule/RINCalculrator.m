e12ea3ed0335939c68f28eb99f78eac679374f25
Hanjong Ko <rino0601@Hanjong-ui-MacBook-Air.local>
2012-04-20 02:57:20 +0900

    Calculator is almost DONE. just need LINK with DB. - RINo

--- RINCalculratorModule/RINCalculratorModule/RINCalculrator.m
+++ RINCalculratorModule/RINCalculratorModule/RINCalculrator.m
@@ -121,19 +121,193 @@
 	Sign=[SEND currentTitle];
 	[(UIButton *)[self viewWithTag:12] setTitle:Sign forState:UIControlStateNormal];
 }
+- (UIButton *)numberKeyMap:(int) num
+{
+	UIButton *BUTTON;
+	switch (num) {
+		case 0:
+			BUTTON = (UIButton *)[self viewWithTag:22];
+			break;
+		case 1:
+			BUTTON = (UIButton *)[self viewWithTag:15];
+			break;
+		case 2:
+			BUTTON = (UIButton *)[self viewWithTag:16];
+			break;
+		case 3:
+			BUTTON = (UIButton *)[self viewWithTag:17];
+			break;
+		case 4:
+			BUTTON = (UIButton *)[self viewWithTag:8];
+			break;
+		case 5:
+			BUTTON = (UIButton *)[self viewWithTag:9];
+			break;
+		case 6:
+			BUTTON = (UIButton *)[self viewWithTag:10];
+			break;
+		case 7:
+			BUTTON = (UIButton *)[self viewWithTag:1];
+			break;
+		case 8:
+			BUTTON = (UIButton *)[self viewWithTag:2];
+			break;
+		case 9:
+			BUTTON = (UIButton *)[self viewWithTag:3];
+			break;
+		default:
+			break;
+	}
+	return BUTTON;
+}
+- (void) diminishButton:(id)sender
+{
+	[UIView animateWithDuration:0.9 animations:^{
+		sleep(1);
+		[(UIButton *)sender setTransform:CGAffineTransformMakeScale(1, 1)];
+		[(UIButton *)sender setHighlighted:NO];
+	}];
+}
+- (void) inflateButton:(id)sender
+{
+	UIButton *pressed =sender;
+	switch (pressed.tag) {
+		case 1: case 2: case 3:
+		case 8: case 9: case 10:
+		case 15:case 16:case 17:
+		case 22:// numberKeypad
+			[self PressNumberKeypad:sender event:nil];
+			break;
+		case 4://clear
+			[self PressClearButton:sender event:nil];
+			break;
+		case 5: //tutorial
+			[self PressTutorialButton:sender event:nil];
+			break;
+		case 11: case 18: //signs
+			[self PressSignButton:sender event:nil];
+			break;
+		case 23: //answer
+			[self PressAnswerButton:sender event:nil];
+			break;
+		case 24: //submit
+			[self PressSubmitButton:sender event:nil];
+			break;
+		default:
+			break;
+	}
+	[UIView animateWithDuration:0.9 animations:^{
+		[(UIButton *)sender setTransform:CGAffineTransformMakeScale(1.2, 1.2)];
+		[(UIButton *)sender setHighlighted:YES];
+		[NSThread detachNewThreadSelector:@selector(diminishButton:) toTarget:self withObject:sender];
+	}];	
+}
 - (void) PressTutorialButton:(id)sender event:(UIEvent *)e
 {
-	UIButton *SEND = (UIButton *)sender;
+//	UIButton *SEND = (UIButton *)sender;
+	int row1=nv1, row2=nv2, row3=correct;
+	[self PressClearButton:nil event:nil];// why it doesn't work?
+	//row1
+	if(row1<10){
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row1]];
+	}else {
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row1/10]];
+		sleep(1);
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row1%10]];
+	}
+	sleep(1);
+	//sign proc
+	if(row3==row1+row2){
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self viewWithTag:11]];
+	}else {
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self viewWithTag:18]];
+	}
+	sleep(1);
+	//row2
+	if (row2<10) {
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row2]];
+	}else {
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row2/10]];
+		sleep(1);
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row2%10]];
+	}
+	sleep(1);
+	//answer proc
+	[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self viewWithTag:23]];
+	sleep(1);
+	//row3
+	if (row3<10) {
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row3]];
+	}else if(row3 <100){
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row3/10]];
+		sleep(1);
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row3%10]];
+	}else {
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row3/100]];
+		sleep(1);
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:(row3/10)%10]];
+		sleep(1);
+		[NSThread detachNewThreadSelector:@selector(inflateButton:) toTarget:self withObject:[self numberKeyMap:row3%10]];
+	}
+	sleep(1);
+	[self PressClearButton:nil event:nil];
 }
 - (void) PressSubmitButton:(id)sender event:(UIEvent *)e
 {
-	UIButton *SEND = (UIButton *)sender;
+	UITextField *DEbug;
+	UIButton *nv11,*nv12,*nv21,*nv22;
+	UIButton *na1,*na2,*na3;
+//	UIButton *SEND = (UIButton *)sender;
+	bool check[4];
+
+	nv11=(UIButton *)[self viewWithTag:6];
+	nv12=(UIButton *)[self viewWithTag:7];
+	nv21=(UIButton *)[self viewWithTag:13];
+	nv22=(UIButton *)[self viewWithTag:14];
+	na1=(UIButton *)[self viewWithTag:19];
+	na2=(UIButton *)[self viewWithTag:20];
+	na3=(UIButton *)[self viewWithTag:21];
+	DEbug=(UITextField *)[self viewWithTag:25];
+	check[3] = YES;
+	// some variable that meaning type of question from DB = v1
+	/*
+	 if(v1==1){
+	 	if([[(UIButton *)[self viewWithTag:12] titleLabel].text isEqualToString:@"+"]){
+	 		check[3]=YES;
+	 	}else{
+	 		check[3]=NO;
+	 	}
+	 }else{
+	 	if([[(UIButton *)[self viewWithTag:12] titleLabel].text isEqualToString:@"-"]){
+	 		check[3]=YES;
+		}else{
+	 		check[3]=NO;
+	 	}
+	 }
+	 */
+	check[0] = nv1==[[nv11 currentTitle] intValue]*10+[[nv12 currentTitle] intValue]*1;
+	check[1] = nv2==[[nv21 currentTitle] intValue]*10+[[nv22 currentTitle] intValue]*1;
+	check[2] = correct==[[na1 currentTitle] intValue]*100 + [[na2 currentTitle] intValue]*10 + [[na3 currentTitle] intValue]*1;
+	if(check[0]&&check[1]&&check[2]&&check[3]){
+		DEbug.text=[NSString stringWithFormat:@"your answer is %d , it is correct",[[na1 currentTitle] intValue]*100 + [[na2 currentTitle] intValue]*10 + [[na3 currentTitle] intValue]*1];
+	}else {
+		DEbug.text=[NSString stringWithFormat:@"your answer is %d , it isn't correct",[[na1 currentTitle] intValue]*100 + [[na2 currentTitle] intValue]*10 + [[na3 currentTitle] intValue]*1];
+	}
 }
 - (void) PressAnswerButton:(id)sender event:(UIEvent *)e
 {
 	UIButton *SEND = (UIButton *)sender;
 	[(UIButton *)[self viewWithTag:SEND.tag] setTitle:@"Check" forState:UIControlStateNormal];
 }
+- (void) generateQuestion:(int) dbID
+{
+	// for test. this method uncompleted.
+	nv1 = 10;
+	nv2 = 30;
+	correct = 40;		// some variable that meaning type of question from DB = v1
+	((UITextField *)[self viewWithTag:25]).text=[NSString stringWithFormat:@"%d + %d = ?",nv1,nv2];
+}
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -158,7 +332,7 @@
 			}
 		}
 		buttonPos.origin.x=2*RIN_GAP_HORIZON+RIN_BTN_WIDTH;
-		[self addSubview:[self addButton:titles[21] parrentBound:buttonPos Tag:20]];
+		[self addSubview:[self addButton:titles[21] parrentBound:buttonPos Tag:22]];
 		buttonPos.origin.x+=2*RIN_GAP_HORIZON+2*RIN_BTN_WIDTH;
 		[self addSubview:[self addButton:titles[22] parrentBound:buttonPos Tag:23]];
 		buttonPos.origin.x+=RIN_GAP_HORIZON+RIN_BTN_WIDTH;
@@ -198,6 +372,7 @@
 			}
 		}
 		[self PressClearButton:self event:nil];
+		[self generateQuestion:0];// for test.
     }
 	
     return self;
